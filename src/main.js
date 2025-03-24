70ad32a47020cb51014e6c7d3d5d4b284b3341ab
Antonyj26 <antonyjosu26@gmail.com>
2025-03-24 02:18:07 -0300

    Alterando input de seleção de horário

--- src/main.js
+++ src/main.js
@@ -9,24 +9,5 @@ import "./styles/form.css";
 import "./styles/header.css";
 import "./styles/schedules.css";
 
-// Seleciona as tags HTML
-const ContainerButtonNewSchedule = document.querySelector(
-  ".button-new-schedule"
-);
-const buttonNewSchedule = document.querySelector(".button-new-schedule button");
-const formDisplay = document.querySelector(".container-form");
-
-buttonNewSchedule.addEventListener("click", (event) => {
-  event.preventDefault();
-
-  if (
-    formDisplay.style.display === "none" ||
-    formDisplay.style.display === ""
-  ) {
-    formDisplay.style.display = "flex";
-    ContainerButtonNewSchedule.style.display = "none";
-  } else {
-    formDisplay.style.display = "none";
-    ContainerButtonNewSchedule.style.display = "flex";
-  }
-});
+//JS
+import "./form/newSchedule.js";
